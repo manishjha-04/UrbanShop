e1ddc49c87004fb48fe7b12136dadfb91132008e
manishjha-04 <stayconnectedwithmanish@gmail.com>
2023-12-30 19:15:15 +0530

    added star and review component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
     <Link to={`/product/${product._id}`}>
         <Card.Img variant="top" src={product.image} />
-    </Link>
+    </Link> 
     <Card.Body>
         <Link to={`/product/${product._id}`}>
-            <Card.Title as="div">
+            <Card.Title as="div" className='product-title'>
             <strong>{product.name}</strong>
             </Card.Title>
         </Link>
+        <Card.Text as='div'>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
         <Card.Text as="h3">${product.price} </Card.Text>
     </Card.Body>
 
