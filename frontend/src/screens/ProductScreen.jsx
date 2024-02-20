cac7a238f3ff640264cb1d1b71dbe884a204836b
manishjha-04 <stayconnectedwithmanish@gmail.com>
2024-02-20 22:42:54 +0530

    product api slice endpoint redux

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { useEffect,useState } from 'react';
+
 // import products from '../products';
-import axios from 'axios';
+
 import { Link } from 'react-router-dom';
 import { Row,Col,Image,ListGroup,Card,Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import { useGetProductDetailsQuery } from '../slices/prouctsApiSlice';
 
 const ProductScreen = () => {
 
-    const [product,setProduct] = useState({});
+    // const [product,setProduct] = useState({});
     const {id: productId} = useParams();
+    const {data:product , isLoading , error} = useGetProductDetailsQuery(productId); 
 
-    useEffect(()=>{  const fetchProduct = async ()=>{
-        const {data} = await axios.get(`/api/products/${productId}`);
-        setProduct(data);
-    }
-    fetchProduct();
 
-},[productId]);
+//     useEffect(()=>{  const fetchProduct = async ()=>{
+//         const {data} = await axios.get(`/api/products/${productId}`);
+//         setProduct(data);
+//     }
+//     fetchProduct();
+
+// },[productId]);
 
 
     // console.log(product);
@@ -27,7 +30,10 @@ const ProductScreen = () => {
       <Link className='btn btn-light my-3' to='/' >
         Go Back
       </Link>
-      <Row>
+      {isLoading ? (<h2>Loading ...</h2>) : error ? (
+        <div> {error?.data?.message || error.error}</div>
+      ) : (
+        <Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -81,6 +87,8 @@ const ProductScreen = () => {
             </Card>
         </Col>
       </Row>
+      ) }
+
     </>
   )
 }
--- frontend/src/slices/prouctsApiSlice.js
+++ frontend/src/slices/prouctsApiSlice.js
@@ -9,10 +9,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor:5 , //used fo testing purposes to see how long data is kept in cache after it's no longer used
         }),
+        getProductDetails : builder.query ({
+            query : (productId) => ({
+                url : `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
+        })
        
     }),
 });
 
 //there is a special convention for exporting data like we use "use " at start and since its a query so query at last
 
-export const {useGetProductsQuery} = productsApiSlice;
+export const {useGetProductsQuery, useGetProductDetailsQuery} = productsApiSlice;
